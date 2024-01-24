5a627295dba2ec529637f3446e939f8acf2ff647
Xeli Vargas <xeli.vargas@uteca.edu.mx>
2024-01-24 14:49:26 -0600

    try error

--- lib/poly.py
+++ lib/poly.py
@@ -5,5 +5,61 @@ class poligono:
         pass
 
     def __str__(self):
-        return {"Numero de lados: {self.numLado}"} y 
-        tamaño del lado: {self.sizeLado}
+        return f"Numero de lados: {self.numLado} y  tamaño del lado: {self.sizeLado}"
+                
+    def nomPoly( self ):
+        match self.numLado:
+            case 3:
+                return"Tu poligono es un triangulo"
+            case 4:
+                return"Tu poligono es un cuadrado"
+            case 5:
+                return"Tu poligono es un pentagono"
+            case 6:
+                return"Tu poligono es un hexagono"
+            case 7:
+                return"Tu poligono es un heptagono"
+            case 8:
+                return"Tu poligono es un octagono"
+            case 9:
+                return"Tu poligono es un enagono"
+            case 10:
+                return"Tu poligono es un decagono"
+            case _:
+                return"Tu poligono no lo identifico "
+            
+            
+            
+    def periPoly(self):
+        return self.numLado * self.sizeLado
+    
+
+        
+##fin clase poligono 
+    
+    
+##inicio clase poligono regular
+
+class PoligonoRegular( poligono ):
+    def __init__(self,numLado , sizeLado, apotema):
+        super().__init__(numLado,sizeLado)
+        self.apotema = apotema
+        pass 
+    def __str__(self):
+        return f"numLado:{self.numLado},sizeLado:{self.sizeLado}, apotema:{self.apotema}"
+    
+    def getArea (self):
+        area= ((self.apotema)*(super().periPoly()))/2
+        return area
+    
+    def chkArea(self):
+        if self.getArea() >= 200:
+            return "si"
+        else :
+            return "no"
+
+    def setcolor(self,color):
+        self.color=color
+        pass
+        #return f"el color es :{self.color}"
+    
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from lib import *
 
 
-obj_rect= rectangulo(10,5)
-print (obj_rect)
-print(obj_rect.ancho)
+#obj_rect= rectangulo(10,5)
+#print (obj_rect)
+#print(obj_rect.ancho)
 
-print("ancho:"+str(obj_rect.ancho))    
-print("largo:"+str(obj_rect.largo))
+#print("ancho:"+str(obj_rect.ancho))    
+#print("largo:"+str(obj_rect.largo))
 
-obj_rect2= rectangulo(25,15)
-print (obj_rect2)
-print(obj_rect2.ancho)
-
-print("ancho:"+str(obj_rect2.ancho))    
+#obj_rect2= rectangulo(25,15)
+#print (obj_rect2)
+#print(obj_rect2.ancho)
+"""
+#print("ancho:"+str(obj_rect2.ancho))    
 print("largo:"+str(obj_rect2.largo))
 
 
@@ -23,4 +23,39 @@ print("ancho:"+str(obj_rect3.ancho)+"[cm]")
 print("largo:"+str(obj_rect3.largo)+"[cm]")
 
 print("perimetro rec1:"+str(obj_rect.perimetro()))
-print("area rec1:"+str(obj_rect.area()))
+print("area rec1:"+str(obj_rect.area()))"""
+""""""###
+
+
+obj_poly_1=poligono(5,18)
+print(obj_poly_1)
+print(f"num Lados:{obj_poly_1.numLado}")
+print(obj_poly_1.nomPoly())
+
+
+
+obj_poly_2=PoligonoRegular(5,10,15)
+print(obj_poly_2)
+print(obj_poly_2.nomPoly())
+print(obj_poly_2.periPoly())
+print(f"el area de un poligono :{obj_poly_2.getArea()}")
+print(f"el area es mayor a 200??{obj_poly_2.chkArea()}")
+obj_poly_2.setcolor("azul")
+print(obj_poly_2.color)
+
+#    ------------------------------------------------
+ZeroDivisionError
+num1 =10
+num2=0
+
+try:
+    div = num1/num2
+#"""except ZeroDivisionError:
+   # print("no puedes dividir entre 0 ")
+#except TypeError :
+   # print("no puedes dividir str entre numeros ")"""
+except Exception as e:
+    print(f"error desconocido:{e}")
+
+
+#----------------------------------------
